1ed99962a4f6a23619c89d807fec02f10efded87
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-03-17 22:17:35 +0800

    21-3-17

---
+++ 21-3-17/src/hello.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.net.Inet4Address;
+
+////public class hello{
+////    public static void main(String[] args) {
+////        long a=10L;
+////        System.out.println(a);
+////    }
+////}
+////public class hello {
+////    public static void main(String[] args) {
+////        float f=12.3F;
+////        System.out.println(f);
+////    }
+////    public static void main1(String[] args) {
+////        double d =112.5;
+////        System.out.println(d);
+////        System.out.println(Double.MAX_VALUE);
+////        System.out.println(Double.MIN_VALUE);
+////
+////    }
+////public static void main(String[] args) {
+////    char a=97;
+////    System.out.println(a);
+////    byte c='a';
+////    System.out.println(c);
+////    boolean aaa=true;
+////    System.out.println(5>10);
+////}
+////}
+//public class hello {
+//    public static void main(String[] args) {
+//        String str="gxhcjhmn";
+//        System.out.println(str);
+//    }
+////}
+//public class hello{
+//    public static void main(String[] args) {
+//
+//    }
+//}
+public class hello {
+    public static void main(String[] args) {
+        final int a=10;
+    }
+}
